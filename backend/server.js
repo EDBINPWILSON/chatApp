2618417b25bd605eb7d7d982239033aa1fb53b2f
Edbin wilson <edbinpwilson@gmail.com>
2024-08-27 13:08:37 +0530

    full-app-completed

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 import connectToMOngoDb from './db/connectToMongoDb.js';
+import { app, server } from './socket/socket.js';
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -22,7 +22,7 @@ app.use('/api/users', userRoutes);
 //   res.send('hello world!');
 // });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMOngoDb();
   console.log(`server is running on port ${PORT}`);
 });
